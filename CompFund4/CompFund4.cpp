ab3d6057415c36c49e3874b979f323964a08ccfd
dell <you@example.com>
2025-12-06 20:13:07 +0200

    Add project files.

---
+++ CompFund4/CompFund4.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num0 = 0;
+    int Largest = 0;
+    int Smallest = 9;
+    int Sum = 0;
+    int n;
+    cout << "Enter a number between 999 and 9999999: ";
+    cin >> n;
+    if (n >= 999 && n <= 9999999) {
+        while (n > 0) {
+            Sum = Sum + n % 10;
+            int digit = n % 10;
+            n = n / 10;
+            if (digit > Largest) {
+                Largest = digit;
+            }
+
+            if (digit < Smallest) {
+                Smallest = digit;
+            }
+            if (digit == 0) {
+                num0++;
+
+            }
+        }
+
+
+        cout << "Sum = " << Sum << endl;
+        cout << "largest = " << Largest << endl;
+        cout << "Smallest = " << Smallest << endl;
+        cout << "Number of 0 digit = " << num0;
+    }
+    else cout << "Invalid input";
+    return 0;
+}
+
